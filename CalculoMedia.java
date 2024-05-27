4238c01bc6e5c7b73c8b3c5369bb6c1d66b9bee3
Carlos Tavares <cmmt08@gmail.com>
2024-05-27 08:46:34 -0300

    Exercicio EBAC

---
+++ CalculoMedia.java
@@ -0,0 +1,15 @@
+public class CalculoMedia{
+    public static void main(String[] args){
+        calculomedia();
+    }
+
+    public static void calculomedia(){
+        double nota1 = 8.5;
+        double nota2 = 6.0;
+        double nota3 = 7.5;
+        double nota4 = 9.0;
+
+        double soma = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println(soma);
+    }
+}
