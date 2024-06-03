98fe863a131225389ba4f476063f75f915bbcdb8
Carlos Tavares <cmmt08@gmail.com>
2024-06-03 17:19:19 -0300

    fix: casting

--- Casting.java
+++ Casting.java
@@ -1,8 +1,7 @@
 public class Casting {
     public static void main(String[] args){
         int numero = 10;
-        System.out.println(numero);
-        Integer numero2 = 20;
-        System.out.println(numero2);
+        Integer numeroWrapper = Integer.valueOf(numero);
+        System.out.println(numeroWrapper);
     }
 }
