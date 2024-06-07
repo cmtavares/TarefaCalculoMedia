f7c32f8022115cc0f291ffc36ce8ba0276c2a8b6
Carlos Tavares <cmmt08@gmail.com>
2024-06-07 14:49:38 -0300

    TrabalhoEBAC

---
+++ ifelse.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ifelse {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Qual é a sua idade: ");
+        int idade = s.nextInt();
+        String idadeSt = getIdade(idade);
+        System.out.println(idadeSt);
+    }
+
+    public static String getIdade(int idade){
+        if (idade >= 0 && idade <= 5){
+            return "Você é um bebê";
+        } else if (idade >= 6 && idade <= 10) {
+            return "Você é uma criança";
+        } else if (idade >= 11 && idade < 18 ) {
+            return "Você é um adolescente";
+        } else {
+            return "Você é um adulto";
+        }
+    }
+}
